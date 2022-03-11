f0ce295766c3fda9d5e828da02757a652ee430e1
Shuai <zengs@umsystem.edu>
2022-03-10 23:45:56 -0600

    update files.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-# FROM centos/systemd
-FROM httpd:2.4
+FROM php:7.4-apache
 
 WORKDIR /var/www/html/
--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-
-	$uri .= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-	header('Location: '.$uri.'shuaicv/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<?php phpinfo(); ?> 
